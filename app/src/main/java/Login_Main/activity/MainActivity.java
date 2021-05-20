14e64c5536b9a62d05da33863a09f1b5107fd40a
Yonjae <yj12300kim@gmail.com>
2021-05-20 17:26:05 +0900

    drawer 나오게 java 수정 -> 크기 조정, 아이콘 수정 필요 & 화면에 보이는 area 조정해야 할 듯

--- app/src/main/java/Login_Main/activity/MainActivity.java
+++ app/src/main/java/Login_Main/activity/MainActivity.java
@@ -1,10 +1,19 @@
 package Login_Main.activity;
 
 import android.content.Intent;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
 
 import Closet.activity.Closet_MainActivity;
 //import petrov.kristiyan.colorpicker_sample.R;
@@ -31,10 +40,25 @@ public class MainActivity extends AppCompatActivity{
     Button mImageSelect;
     Button mLookBook;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
     private Intent intent;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         mLogoutButton = (Button) findViewById(R.id.logout_button);
         mLogoutButton.setOnClickListener(new View.OnClickListener() {
@@ -169,5 +193,7 @@ public class MainActivity extends AppCompatActivity{
 
          */
     }
+
+
 
 }
